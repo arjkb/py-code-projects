0252db9f9443edb25e922e95746a619c2742da29
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-01 12:09:54 -0700

    scrape information from default location of the information provider

---
+++ ip-geo-locator/ip-geo-locator.py
@@ -0,0 +1,43 @@
+import argparse
+import requests
+
+from bs4 import BeautifulSoup
+
+
+
+def geo_locate(ip):
+    try:
+        r = requests.get('https://www.iplocation.net/')
+    except Exception:
+        return "Failed to establish connection"
+
+    if r.status_code != requests.codes.ok:
+        return "not available"
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    td = soup.find_all("td")
+
+    d = dict()
+    d['ip'] = td[0].string
+    d['country'] = td[1].string
+    d['region'] = td[2].string
+    d['city'] = td[3].string
+    d['isp'] = td[4].string
+    d['organization'] = td[5].string
+
+    # print(td[0].string) # ip
+    # print(td[1].string) # Country
+    # print(td[2].string) # Region
+    # print(td[3].string) # City
+    # print(td[4].string) # ISP
+    # print(td[5].string) # Organization
+    return d
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ip", help="ip address you wish to locate", type=str)
+    args = parser.parse_args()
+
+    location = geo_locate(args.ip)
+
+    print(" City: ", location['city'])
