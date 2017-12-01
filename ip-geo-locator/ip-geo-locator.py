82cd515363606794e8fb173771e1791bc9cdcf53
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-01 12:45:31 -0700

    remove obsolete code

--- ip-geo-locator/ip-geo-locator.py
+++ ip-geo-locator/ip-geo-locator.py
@@ -14,27 +14,7 @@ def geo_locate(ip):
 
     if r.status_code != requests.codes.ok:
         return "not available"
-
-    # soup = BeautifulSoup(r.text, 'html.parser')
-    # td = soup.find_all("td")
-
-    # d = dict()
-    # d['ip'] = td[0].string
-    # d['country'] = td[1].string
-    # d['region'] = td[2].string
-    # d['city'] = td[3].string
-    # d['isp'] = td[4].string
-    # d['organization'] = td[5].string
-
-
-    # print(td[0].string) # ip
-    # print(td[1].string) # Country
-    # print(td[2].string) # Region
-    # print(td[3].string) # City
-    # print(td[4].string) # ISP
-    # print(td[5].string) # Organization
-    # # return d
-    # return ""
+    
     return r.json()
 
 if __name__ == '__main__':
