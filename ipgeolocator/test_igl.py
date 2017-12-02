08d39a9444db2ede7fbb2fbbd74c684de24d67ce
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-01 23:56:12 -0700

    test is_valid()

--- ipgeolocator/test_igl.py
+++ ipgeolocator/test_igl.py
@@ -8,5 +8,16 @@ def test_func():
     assert func(4) == 6
 
 def test_isvalid():
-    print(ipgeolocator.is_valid(4))
-    assert (ipgeolocator.is_valid(3) == 6)
+    assert (ipgeolocator.is_valid('192.168.4.5') == True)
+    assert (ipgeolocator.is_valid('0.0.0.0') == True)
+    assert (ipgeolocator.is_valid('255.255.255.255') == True)
+
+    assert (ipgeolocator.is_valid('0.0.0.256') == False)
+    assert (ipgeolocator.is_valid('0.0.256.0') == False)
+    assert (ipgeolocator.is_valid('0.256.0.0') == False)
+    assert (ipgeolocator.is_valid('256.0.0.0') == False)
+    assert (ipgeolocator.is_valid('100.200.300.400') == False)
+
+    assert (ipgeolocator.is_valid('256.256.256.256') == False)
+    assert (ipgeolocator.is_valid('-1,-1,-1,-1') == False)
+    assert (ipgeolocator.is_valid('192.168') == False)
